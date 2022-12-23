be6924dd0c3ef32751f0754085a2ea27ea9563f7
Uzair Sharif <uzleo.sharif@gmail.com>
2022-12-24 00:47:36 +0100

    configurability

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -7,30 +7,29 @@ macro(utils_conan_install)
   include(${CMAKE_SOURCE_DIR}/cmake/conan.cmake)
 
   foreach(pkg ${conan_required_packages})    
-    # # for multi-config build system generator
-    # conan_cmake_configure(REQUIRES ${pkg} GENERATORS cmake_find_package_multi)
-    # foreach(TYPE ${CMAKE_CONFIGURATION_TYPES})
-    #   conan_cmake_autodetect(settings BUILD_TYPE ${TYPE})      
-    #   conan_cmake_install(
-    #     PATH_OR_REFERENCE .
-    #     BUILD missing
-    #     REMOTE conancenter
-    #     SETTINGS ${settings}
-    #     CONF "tools.system.package_manager:mode=install"
-    #   )
-    # endforeach()
-    
-    
-    # for single-config build system generator
-    conan_cmake_configure(REQUIRES ${pkg} GENERATORS cmake_find_package)
-    conan_cmake_autodetect(settings)
-    conan_cmake_install(
-      PATH_OR_REFERENCE .
-      BUILD missing
-      REMOTE conancenter
-      SETTINGS ${settings}
-      CONF "tools.system.package_manager:mode=install"      
-    )
+    if(${is_multi_config})
+      conan_cmake_configure(REQUIRES ${pkg} GENERATORS cmake_find_package_multi)
+      foreach(TYPE ${CMAKE_CONFIGURATION_TYPES})
+        conan_cmake_autodetect(settings BUILD_TYPE ${TYPE})      
+        conan_cmake_install(
+          PATH_OR_REFERENCE .
+          BUILD missing
+          REMOTE conancenter
+          SETTINGS ${settings}
+          CONF "tools.system.package_manager:mode=install"
+        )
+      endforeach()
+    else()
+      conan_cmake_configure(REQUIRES ${pkg} GENERATORS cmake_find_package)
+      conan_cmake_autodetect(settings)
+      conan_cmake_install(
+        PATH_OR_REFERENCE .
+        BUILD missing
+        REMOTE conancenter
+        SETTINGS ${settings}
+        CONF "tools.system.package_manager:mode=install"      
+      )
+    endif()
   endforeach()
   
 endmacro()
