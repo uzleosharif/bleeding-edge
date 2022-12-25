7411bd90b1f7703fec758889d9a7b5eba5b861de
uzleo <uzleo_eth@proton.me>
2022-12-25 04:16:21 +0100

    more clarity on -1

--- src/app/qt-tutorials/README.md
+++ src/app/qt-tutorials/README.md
@@ -1,3 +1,13 @@
 
 
 tutorials to learn Qt framework for building GUIs for apps
+
+# notes
+- the generated executables rely on dynamic linkage (e.g. to conan installed shared libs). To run the binary we first 
+  have to guide the loader about where to find these libs. For this, we can use 
+  ```
+  $ objdump -p <elf>
+  # then simply put the `RUNPATH` to `LD_LIBRARY_PATH`
+  $ export LD_LIBRARY_PATH=<copy-pasta of RUNPATH>:${LD_LIBRARY_PATH}
+  $ ./<elf>
+  ```
--- src/app/qt-tutorials/gui_hello.cpp
+++ src/app/qt-tutorials/gui_hello.cpp
@@ -3,13 +3,13 @@
 #include "QtWidgets/QApplication"
 #include "QtWidgets/QWidget"
 
-auto main(int argc, char **argv) -> int {
-    QApplication app(argc, argv);
+auto main(int argc, char** argv) -> int {
+  QApplication app(argc, argv);
 
-    QWidget window{};
-    window.resize(250, 150);
-    window.setWindowTitle("simple example");
-    window.show();
+  QWidget window{};
+  window.resize(250, 150);
+  window.setWindowTitle("simple example");
+  window.show();
 
-    return app.exec();
+  return app.exec();
 }
