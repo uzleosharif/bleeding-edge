1d7b4488ffc8dc7dd6e63356a803958de147e36c
uzleo <uzleo_eth@proton.me>
2022-12-29 11:31:21 +0100

    wip

--- src/app/qt-tutorials/gui_hello.cpp
+++ src/app/qt-tutorials/gui_hello.cpp
@@ -4,7 +4,7 @@
 #include "QtWidgets/QWidget"
 
 auto main(int argc, char** argv) -> int {
-  QApplication app(argc, argv);
+  QApplication app{argc, argv};
 
   QWidget window{};
   window.resize(250, 150);
